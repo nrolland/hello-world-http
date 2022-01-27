c88fc02f323640dd23d452fbc1708844fef6a212
Nicolas Rolland <nrolland@users.noreply.github.com>
2022-01-27 15:14:16 +0100

    Update helloworldhs.sh

--- helloworldhs.sh
+++ helloworldhs.sh
@@ -1,1 +1,1 @@
- $(nix-build -E "with import <nixpkgs> { };haskellPackages.ghcWithPackages(p: with p; [network HTTP conduit conduit-extra])")/bin/runghc -v helloworld.hs
+nix shell --impure --expr "with import <nixpkgs> { };haskellPackages.ghcWithPackages(p: with p; [network HTTP conduit conduit-extra])" --command runghc -v helloworld.hs
